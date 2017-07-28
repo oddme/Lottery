8346b56b6e12960d114f169f9fbfe970ae11496d
zhuzhou <oddmezhu@gmail.com>
2017-07-28 13:43:29 +0800

    update format

---
+++ js/start.js
@@ -0,0 +1,14 @@
+/**
+ * Created by admin on 2017/7/28.
+ */
+let app = document.getElementById('app');
+let lottery = new Lottery([{'url': 'images/btn.png', 'text': '开始按钮'},
+    {'url': 'images/01.png', 'text': '恭喜你抽中1元现金红包'},
+    {'url': 'images/02.png', 'text': '恭喜你抽中2元现金红包'},
+    {'url': 'images/03.png', 'text': '恭喜你抽中3元现金红包'},
+    {'url': 'images/04.png', 'text': '恭喜你抽中4元现金红包'},
+    {'url': 'images/05.png', 'text': '恭喜你抽中5元现金红包'},
+    {'url': 'images/06.png', 'text': '恭喜你抽中6元现金红包'},
+    {'url': 'images/07.png', 'text': '恭喜你抽中7元现金红包'},
+    {'url': 'images/08.png', 'text': '恭喜你抽中8元现金红包'}], 3, app);
+lottery.start();
